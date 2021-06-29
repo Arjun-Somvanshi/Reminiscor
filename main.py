e2ee4a67832f28dad3fd8f326c673d7ba49d1cd1
Arjun-Somvanshi <arjunsomvanshi@gmail.com>
2021-06-30 01:22:13 +0530

    added the task bar to provide additional sorting and finding capabalities

--- main.py
+++ main.py
@@ -25,12 +25,13 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp, sp
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 from kivy.uix.modalview import ModalView
 from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from functools import partial
-from kivy.properties import ListProperty, NumericProperty, StringProperty, ObjectProperty, BooleanProperty
+from kivy.properties import ListProperty, NumericProperty, StringProperty, ObjectProperty, BooleanProperty, OptionProperty
 from response import *
 from kivy.logger import Logger, LOG_LEVELS
 Logger.setLevel(LOG_LEVELS["debug"])
@@ -60,6 +61,86 @@ def quickmessage(title, message, *args):
 '''-------------------------------------------------------'''
 
 '''-----------------Custom Classes-----------------------'''
+class UIDropDownItem(Button):
+    owner = ObjectProperty()
+    dd_object = ObjectProperty()
+    rounded = BooleanProperty(False)
+    item_color = ListProperty([[1,1,1,1], [1,1,1,0.5]])
+    item_font = StringProperty('Roboto')
+    item_font_size = NumericProperty(sp(15))
+    item_font_color = ListProperty([0,0,0,1])
+    background_img_normal = StringProperty('')
+    background_img_down = StringProperty('')
+    def on_release(self):
+        self.owner.selected_item = self.text
+        self.dd_object.dropdown_view.dismiss()
+
+class UIDropDownContent(BoxLayout):
+    Elements = ListProperty([]) 
+    selected_item = StringProperty('')
+    background_color = ListProperty([0,0,0,1])
+    item_height = NumericProperty(dp(30))
+    item_size_hint_x = NumericProperty(1)
+    def __init__(self, dropdown_instance, elements: list, background_color: list, item_height = dp(30), **kwargs):
+        super(UIDropDownContent, self).__init__(**kwargs)
+        for element in elements:
+            self.Elements.append({'text': element, 
+                                  'owner': self, 
+                                  'dd_object': dropdown_instance, 
+                                  'item_color': dropdown_instance.dd_item_color,
+                                  'background_img_down': dropdown_instance.dd_item_img_down,
+                                  'background_img_normal': dropdown_instance.dd_item_img_normal,
+                                  'rounded': dropdown_instance.dd_item_isrounded,
+                                  'pos_hint': {'center_x': 0.5}, # this is how you would center widgets inside a recycle view
+                                  'item_font': dropdown_instance.dd_item_font,
+                                  'item_font_color': dropdown_instance.dd_item_font_color,
+                                  'item_font_size': dropdown_instance.dd_item_font_size
+                                  })
+            self.background_color = background_color
+            self.item_height = item_height
+            self.item_size_hint_x = dropdown_instance.dd_item_size_hint_x
+
+class UIDropDown(Button):
+    dd_open_vertical = OptionProperty('down', options = ['up', 'down'])
+    dd_open_horizontal = OptionProperty('center', options = ['left', 'center', 'right'])
+    def calculate_minimum_height(self):
+        required_height = 0
+        for i in range(self.show_items):
+            required_height += self.dd_item_height + dp(15)
+        return required_height + dp(15)
+
+    def set_dropdown(self):
+        self.content = UIDropDownContent(self, self.elements, self.dd_background_color, self.dd_item_height)
+        if self.calculate_ddheight:
+            self.dd_size[1] = self.calculate_minimum_height()
+        self.dropdown_view = DropDownModalView(
+                                           size_hint = (None, None),
+                                           size = self.dd_size,
+                                           overlay_color = (0,0,0,0)
+                                       )
+        if self.dd_open_vertical == 'down':
+            vertical =  ['top',self.y/Window.height]
+        elif self.dd_open_vertical == 'up':
+            vertical = ['y', self.top/Window.height]
+        if self.dd_open_horizontal == 'center':
+            horizontal = ['center_x', self.center_x/Window.width]
+        elif self.dd_open_horizontal == 'left':
+            horizontal  = ['x', self.x/Window.width]
+        elif self.dd_open_horizontal == 'right':
+            horizontal = ['right', self.right/Window.width]
+        self.dropdown_view.pos_hint = {horizontal[0]: horizontal[1], vertical[0]: vertical[1]}
+        self.dropdown_view.add_widget(self.content)
+        self.dropdown_view.bind(on_dismiss = self.set_selected_item)
+        Window.bind(on_resize=partial(self.dropdown_view.dismiss, True))
+    
+    def on_release(self):
+        self.set_dropdown()
+        self.dropdown_view.open(self.dd_size[1])
+
+    def set_selected_item(self, instance):
+        if self.content.selected_item:
+            self.text = self.content.selected_item
+
 class CustomTextInput(TextInput):
     def on_parent(self, *_):
         self._refresh_text(self.text)
@@ -130,6 +211,28 @@ class CustomModalView(ModalView): # here I have made a custom modal view so that
 
     def finish_dismiss(self, *args):
         super(CustomModalView, self).dismiss()
+
+class DropDownModalView(ModalView):
+    def open(self, final_height, *largs, **kwargs):
+        self.height = 0
+        self.opacity = 0
+        anim = Animation(height = final_height, t = 'in_expo', duration = 0.4)
+        anim &= Animation(opacity=1, t='in_expo', duration = 0.4)
+        anim.start(self)
+        super(DropDownModalView, self).open(*largs, **kwargs)
+    
+    def dismiss(self, fast = False, *largs, **kwargs):
+        if not fast:
+            anim = Animation(height = 0, t='out_expo', duration=0.4)
+            anim &= Animation(opacity = 0, t='out_expo', duration=0.4)
+            anim.start(self)
+            anim.bind(on_complete = self.finish_dismiss)
+        else:
+            super(DropDownModalView, self).dismiss()
+    
+    def finish_dismiss(self, *args):
+        super(DropDownModalView, self).dismiss()
+
 '''------------------------------------------------------'''
 
 
@@ -308,11 +411,13 @@ class Login(Screen):
                     with open(HomeDir('database.json', 'UserData')) as f:
                         encrypted_data =  json.load(f)
                     app.database = AES_Decrypt(result[1], encrypted_data)
+                    # unassigning the derived master key from the result
+                    result = [1,2,''] # random reassignment of the list 
                 except:
                     pass
                 self.transition()
             else:
-                quickmessage('Login Error', 'The Master Password is [color=#a93226]wrong.[/color]')
+                quickmessage('Login Error', 'The Master Password is [color=#a93226] wrong.[/color]')
 
     def transition(self, *args):
         app.root.transition = FadeTransition(duration=0.5)
@@ -343,7 +448,7 @@ class Main(Screen):
         self.manager.current = 'login'
     def navigation(self):
         global app
-        nav_content = NavigationView()
+        nav_content = NavigationView() 
         self.nav = CustomModalView(
                             size_hint = (0.5, 1),
                             size_hint_max = (dp(250), None),
