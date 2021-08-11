8cb56ee981c4c7a01f14c9b1ddd4c485756ba7df
Arjun-Somvanshi <arjunsomvanshi@gmail.com>
2021-08-11 22:36:39 +0530

    added signup response

---
+++ SignupResponse.py
@@ -0,0 +1,15 @@
+from FileHandling import *
+# This compares password and confirm password
+
+
+def CheckMainPassword(usrName, paswrd, repaswrd):
+
+    # Change minimum password size to 12
+    if paswrd == repaswrd and len(usrName) >= 8 and len(usrName) <= 50 and len(paswrd) >= 8 and paswrd != usrName:
+        for i in usrName:
+            if i == ',':
+                return False
+            else:
+                return True
+    else:
+        return False
